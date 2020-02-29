624a437ade31dbd33cded9b7722919855c1c1ca2
maayson <polishyk36@gmail.com>
2020-02-29 12:31:38 +0200

    day19

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,20 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('blog.index');
+})->name('/');
+
+Route::get('/about', function () {
+    return view('blog.about');
+})->name('about');
 
 Route::get('/post', function () {
-    return ('welcome');
-})->middleware('auth');
+    return view('blog.post');
+})->name('post');
 
+Route::get('/contact', function () {
+    return view('blog.contact');
+})->name('contact');
 
 Route::get('/about, AboutController@index');
 Auth::routes();
